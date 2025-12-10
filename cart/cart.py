c9d7fa303e43ade55e249f2ba1625aac58e615ad
fidelis182 <fedymueni18@gmail.com>
2025-12-10 09:03:49 +0300

    created add to cart function

--- cart/cart.py
+++ cart/cart.py
@@ -13,4 +13,13 @@ class Cart():
     self.cart=cart
 
 
-  
+  def add(self, product):
+    product_id = str(product.id)   # ← MUST be inside the method
+    if product_id in self.cart:
+            pass
+    else:
+     self.cart[product_id] = {
+        'price': str(product.price),
+      }
+
+    self.session.modified = True
--- cart/views.py
+++ cart/views.py
@@ -1,11 +1,27 @@
-from django.shortcuts import render
-
+from django.shortcuts import get_object_or_404, render
+from .cart import Cart
+from woodmat.models import Product
+from django.http import JsonResponse
 # Create your views here.
 def cart_summary(request):
   return render(request,"cart_summary.html")
 
 def cart_add(request):
-  pass
+  #get the cart
+  cart=Cart(request)
+  #test POST
+  if request.POST.get('action')=='post':
+    #get stuff
+    product_id=int(request.POST.get('product_id'))
+    #lookup product inDB
+    product= get_object_or_404(Product,id=product_id)
+    #save to session
+    cart.add(product=product)
+
+    #return response
+    response=JsonResponse({'product Name':product.name})
+    return response
+
 
 def cart_delete(request):
   pass
